997930286cec0dff119607bcd15a53fe7098759f
medridhabenali5-tech <medridhabenali5@gmail.com>
2025-11-20 17:57:42 +0100

    Ajout du site statique personnel
    Déploiement du site de présentation avec sections parcours, centres d'intérêt et activités

---
+++ script.js
@@ -0,0 +1,13 @@
+// Animation simple pour le défilement
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Message de bienvenue
+console.log('Bienvenue sur mon site');
